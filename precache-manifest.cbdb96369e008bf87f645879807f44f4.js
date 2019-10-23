14339ac8ca00a755fc45f7c1bdbd64936e19bea7
codeghoul <jayesh.patel.2197@gmail.com>
2019-10-23 10:45:23 +0530

    Updates

---
+++ precache-manifest.cbdb96369e008bf87f645879807f44f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cbcc5ed7298499f2063b83a76d7522e",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "c98bb03f7928aa837458",
+    "url": "/burger-builder/static/css/main.16cf431d.chunk.css"
+  },
+  {
+    "revision": "5d3a48adf6ab9be26adb",
+    "url": "/burger-builder/static/js/2.3d186d05.chunk.js"
+  },
+  {
+    "revision": "c98bb03f7928aa837458",
+    "url": "/burger-builder/static/js/main.4f5543ea.chunk.js"
+  },
+  {
+    "revision": "01f389611d85dd339f28",
+    "url": "/burger-builder/static/js/runtime-main.fff5d8f9.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
